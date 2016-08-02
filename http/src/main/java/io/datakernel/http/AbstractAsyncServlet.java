15a066efee97d3c878333f3d14fcf05c65c6c785
Vitaly Mykhalko <vmykhalko@datakernel.io>
2016-08-02 17:20:37 +0300

    add distributed jmx stats for servlet errors based on http code

--- http/src/main/java/io/datakernel/http/AbstractAsyncServlet.java
+++ http/src/main/java/io/datakernel/http/AbstractAsyncServlet.java
@@ -20,8 +20,15 @@ import io.datakernel.async.ParseException;
 import io.datakernel.async.ResultCallback;
 import io.datakernel.eventloop.Eventloop;
 import io.datakernel.jmx.*;
+import io.datakernel.time.CurrentTimeProvider;
+import io.datakernel.time.CurrentTimeProviderSystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import static java.util.Arrays.asList;
 
 /**
  * Represent an asynchronous HTTP servlet which receives and responds to requests from clients across HTTP.
@@ -35,11 +42,11 @@ public abstract class AbstractAsyncServlet implements AsyncHttpServlet, Eventloo
 
 	// JMX
 	private final EventStats requests = new EventStats();
-	private final ExceptionStats errors = new ExceptionStats();
+	private final ServletErrorStats errors = new ServletErrorStats();
 	private final ValueStats requestsTimings = new ValueStats();
 	private final ValueStats errorsTimings = new ValueStats();
 
-	// TODO(vmykhaklo): add map:   error code -> url
+	private final Map<Integer, ServletErrorStats> errorCodeToStats = new HashMap<>();
 
 	protected AbstractAsyncServlet(Eventloop eventloop) {
 		this.eventloop = eventloop;
@@ -68,22 +75,58 @@ public abstract class AbstractAsyncServlet implements AsyncHttpServlet, Eventloo
 		}
 		requests.recordEvent();
 		final long timestamp = eventloop.currentTimeMillis();
-		doServeAsync(request, new Callback() {
-			@Override
-			public void onResult(HttpResponse result) {
-				int duration = (int) (eventloop.currentTimeMillis() - timestamp);
-				requestsTimings.recordValue(duration);
-				callback.onResult(result);
-			}
+		try {
+			doServeAsync(request, new Callback() {
+				@Override
+				public void onResult(HttpResponse result) {
+					// jmx
+					int duration = (int) (eventloop.currentTimeMillis() - timestamp);
+					requestsTimings.recordValue(duration);
 
-			@Override
-			public void onHttpError(HttpServletError httpServletError) {
-				int duration = (int) (eventloop.currentTimeMillis() - timestamp);
-				errorsTimings.recordValue(duration);
-				errors.recordException(httpServletError, extractUrl(request));
-				callback.onHttpError(httpServletError);
-			}
-		});
+					callback.onResult(result);
+				}
+
+				@Override
+				public void onHttpError(HttpServletError httpServletError) {
+					// jmx
+					int duration = (int) (eventloop.currentTimeMillis() - timestamp);
+					errorsTimings.recordValue(duration);
+					recordError(httpServletError, request);
+
+					callback.onHttpError(httpServletError);
+				}
+			});
+		} catch (ParseException parseException) {
+			int badRequestHttpCode = 400;
+			HttpServletError error = new HttpServletError(badRequestHttpCode, parseException);
+
+			// jmx
+			int duration = (int) (eventloop.currentTimeMillis() - timestamp);
+			errorsTimings.recordValue(duration);
+			recordError(error, request);
+
+			callback.onHttpError(error);
+		}
+	}
+
+	private void recordError(HttpServletError error, HttpRequest request) {
+		int code = error.getCode();
+		String url = extractUrl(request);
+		Throwable cause = error.getCause();
+		String message = error.getMessage();
+
+		errors.recordError(cause, url, message);
+		ServletErrorStats stats = ensureStats(code);
+		stats.recordError(cause, url, message);
+	}
+
+	private ServletErrorStats ensureStats(int code) {
+		ServletErrorStats stats = errorCodeToStats.get(code);
+		if (stats == null) {
+			stats = new ServletErrorStats(eventloop);
+			errorCodeToStats.put(code, stats);
+		}
+		return stats;
 	}
 
 	// jmx
@@ -92,7 +135,7 @@ public abstract class AbstractAsyncServlet implements AsyncHttpServlet, Eventloo
 	}
 
 	private static String extractUrl(HttpRequest request) {
-		return "url: " + request.toString();
+		return request.toString();
 	}
 
 	@Override
@@ -106,7 +149,7 @@ public abstract class AbstractAsyncServlet implements AsyncHttpServlet, Eventloo
 	}
 
 	@JmxAttribute(description = "requests that were handled with error")
-	public final ExceptionStats getErrors() {
+	public final ServletErrorStats getErrors() {
 		return errors;
 	}
 
@@ -118,6 +161,84 @@ public abstract class AbstractAsyncServlet implements AsyncHttpServlet, Eventloo
 	@JmxAttribute(description = "duration of handling one request in case of error")
 	public final ValueStats getErrorsTimings() {
 		return errorsTimings;
+	}
+
+	@JmxAttribute(description = "servlet errors distributed by http code")
+	public final Map<Integer, ServletErrorStats> getErrorCodeToStats() {
+		return errorCodeToStats;
+	}
+
+	public static final class ServletErrorStats implements JmxStats<ServletErrorStats> {
+		public static final SimpleDateFormat TIMESTAMP_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+		private final CurrentTimeProvider timeProvider;
+
+		private Throwable throwable;
+		private int count;
+		private long lastErrorTimestamp;
+		private String url;
+		private String message;
+
+		public ServletErrorStats(CurrentTimeProvider timeProvider) {
+			this.timeProvider = timeProvider;
+		}
+
+		public ServletErrorStats() {
+			this.timeProvider = new CurrentTimeProviderSystem();
+		}
+
+		public void recordError(Throwable throwable, String url, String message) {
+			this.count++;
+			this.throwable = throwable;
+			this.lastErrorTimestamp = timeProvider.currentTimeMillis();
+			this.url = url;
+			this.message = message;
+		}
+
+		@Override
+		public void add(ServletErrorStats another) {
+			this.count += another.count;
+			if (another.lastErrorTimestamp > this.lastErrorTimestamp) {
+				this.throwable = another.throwable;
+				this.lastErrorTimestamp = another.lastErrorTimestamp;
+				this.url = another.url;
+				this.message = another.message;
+			}
+		}
+
+		@JmxAttribute
+		public int getTotalErrors() {
+			return count;
+		}
+
+		@JmxAttribute
+		public String getLastErrorType() {
+			return throwable != null ? throwable.getClass().getName() : "";
+		}
+
+		@JmxAttribute
+		public String getLastErrorTimestamp() {
+			return TIMESTAMP_FORMAT.format(new Date(lastErrorTimestamp));
+		}
+
+		@JmxAttribute
+		public String getLastErrorUrl() {
+			return url;
+		}
+
+		@JmxAttribute
+		public String getLastErrorMessage() {
+			return message;
+		}
+
+		@JmxAttribute
+		public List<String> getLastErrorStackTrace() {
+			if (throwable != null) {
+				return asList(MBeanFormat.formatException(throwable));
+			} else {
+				return new ArrayList<>();
+			}
+		}
 	}
 
 }
